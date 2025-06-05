6da55fb29a2bfd363677d17f8d59bff218dbb7fa
Mano-ManooI <cyfgghgihh1@gmail.com>
2025-06-05 15:36:34 -0300

    Projeto Reiniciado

---
+++ Mano-ManooI/src/main.js
@@ -0,0 +1,11 @@
+addEventListener("DOMContentLoaded", () =>{
+    document.getElementById("start").addEventListener("click", () =>{
+        window.scrollTo({top: 0, behavior: "smooth"})
+    })
+    document.getElementById("about").addEventListener("click", () =>{
+        window.scrollTo({top: 0, behavior: "smooth"})
+    })
+    document.getElementById("works").addEventListener("click", () =>{
+        window.scrollTo({top: 0, behavior: "smooth"})
+    })
+})
