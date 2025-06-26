be3934e49cda95b5c2e732a09933296a6fee9e29
Mano-ManooI <cyfgghgihh1@gmail.com>
2025-06-26 17:42:17 -0300

    Desisto FrontEnd. Você venceu

---
+++ To Do List/src/script.js
@@ -0,0 +1,37 @@
+const inputTask = document.getElementById("task");
+inputTask.addEventListener("inputTask", () =>{
+    inputTask.value = inputTask.value.replace(/[^A-ZA-zÀ-ú\s]/g, "");
+});
+
+function toDoList(){
+    let task = inputTask.value.trim();
+    task = task.charAt(0).toUpperCase() + task.slice(1).toLowerCase();
+
+    const list = document.getElementById("list");
+    const li = document.createElement("li");
+    li.innerText = task;
+    list.appendChild(li);
+    const removeTask = document.createElement("button");
+    removeTask.innerHTML = `
+        <img src="./img/lixeira 1.png" draggable="false" id="trash1">
+        <img src="./img/lixeira 2.png" draggable="false" id="trash2">
+    `;
+    removeTask.addEventListener("click", () =>{
+        li.remove();
+    });
+    li.appendChild(removeTask);
+
+    inputTask.value = "";
+};
+
+const buttonAddTask = document.getElementById("addTask");
+buttonAddTask.addEventListener("click", (event) =>{
+    event.preventDefault();
+
+    toDoList();
+});
+inputTask.addEventListener("keypress", (e) =>{
+    if(e.key === "Enter"){
+        toDoList();
+    };
+});
