fd28bf4829f75dc3299e24dbaeca0e9b0de4c367
Mano-ManooI <cyfgghgihh1@gmail.com>
2025-11-23 18:45:02 -0300

    Iniciando start

--- Mano-ManooI/src/scrpt.js
+++ Mano-ManooI/src/scrpt.js
@@ -1,8 +1,3 @@
-const logo = document.getElementById("logo");
-logo.addEventListener("click", () =>{
-    open("https://github.com/Mano-ManooI");
-});
-
 const elementToStart = document.getElementById("toStart");
 const elementToAbout = document.getElementById("toAbout");
 const elementToProjects = document.getElementById("toProjects");
@@ -32,5 +27,19 @@ window.addEventListener("scroll", () =>{
 });
 
 elementToStart.addEventListener("click", () =>{
-    window.scrollTo({top: 0, behavior: "smooth"})
-})
+    window.scrollTo({top: 0, behavior: "smooth"});
+});
+
+const phoneNumber = "5579998801174";
+const buttonFindOutMore = document.getElementById("findOutMore");
+buttonFindOutMore.addEventListener("click", () =>{
+    const message = encodeURIComponent("Olá! Tudo bem? Gostaria de gostaria de saber mais sobre você!");
+    const link = `https://wa.me/${phoneNumber}?text=${message}`;
+    window.open(link, "_blank");
+});
+const buttonContact = document.getElementById("buttonContact");
+buttonContact.addEventListener("click", () =>{
+    const message = encodeURIComponent("Olá! Tudo bem? Gostaria de gostaria de conversar com você sobre: ");
+    const link = `https://wa.me/${phoneNumber}?text=${message}`;
+    window.open(link, "_blank");
+});
