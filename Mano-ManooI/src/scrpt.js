f673a33bad25971c4949247b29298a657bf566f1
Mano-ManooI <cyfgghgihh1@gmail.com>
2025-12-15 15:00:42 -0300

    finaização do start

--- Mano-ManooI/src/scrpt.js
+++ Mano-ManooI/src/scrpt.js
@@ -1,3 +1,5 @@
+const header = document.querySelector("header");
+
 const elementToStart = document.getElementById("toStart");
 const elementToAbout = document.getElementById("toAbout");
 const elementToProjects = document.getElementById("toProjects");
@@ -10,15 +12,19 @@ const textContact = "Contato";
 const iconStart = `<i class="fa-solid fa-house"></i>`;
 const iconAbout = `<i class="fa-solid fa-circle-info"></i>`;
 const iconProjects = `<i class="fa-solid fa-briefcase"></i>`;
-const iconContact = `<i class="fa-brands fa-square-whatsapp"></i>`;
+const iconContact = `<i class="fa-brands fa-whatsapp"></i>`;
 
 window.addEventListener("scroll", () =>{
     if(window.scrollY > 150){
+        header.style.width = "25%";
+        header.style.background = "#f9f9f9";
         elementToStart.innerHTML = iconStart;
         elementToAbout.innerHTML = iconAbout;
         elementToProjects.innerHTML = iconProjects;
         elementToContact.innerHTML = iconContact;
     }else{
+        header.style.width = "75%";
+        header.style.background = "rgb(235, 235, 255)";
         elementToStart.innerHTML = textStart;
         elementToAbout.innerHTML = textAbout;
         elementToProjects.innerHTML = textProjects;
