28b2ee629c499651e63544f90e76d6ffd61cc2ee
Mano-ManooI <cyfgghgihh1@gmail.com>
2026-01-21 10:36:08 -0300

    removendo 'works'

--- Mano-ManooI/src/style.js
+++ Mano-ManooI/src/style.js
@@ -2,16 +2,13 @@ const header = document.querySelector("header");
 
 const elementToStart = document.getElementById("toStart");
 const elementToAbout = document.getElementById("toAbout");
-const elementToProjects = document.getElementById("toProjects");
 const elementToContact = document.getElementById("toContact");
 
 const textStart = "Início";
 const textAbout = "Sobre";
-const textProjects = "Projetos";
 const textContact = "Contato";
 const iconStart = `<i class="fa-solid fa-house"></i>`;
 const iconAbout = `<i class="fa-solid fa-circle-info"></i>`;
-const iconProjects = `<i class="fa-solid fa-briefcase"></i>`;
 const iconContact = `<i class="fa-brands fa-whatsapp"></i>`;
 
 window.addEventListener("scroll", () =>{
@@ -20,14 +17,12 @@ window.addEventListener("scroll", () =>{
         header.style.background = "#f9f9f9";
         elementToStart.innerHTML = iconStart;
         elementToAbout.innerHTML = iconAbout;
-        elementToProjects.innerHTML = iconProjects;
         elementToContact.innerHTML = iconContact;
     }else{
         header.style.width = "75%";
         header.style.background = "rgb(235, 235, 255)";
         elementToStart.innerHTML = textStart;
         elementToAbout.innerHTML = textAbout;
-        elementToProjects.innerHTML = textProjects;
         elementToContact.innerHTML = textContact;
     };
 });
@@ -37,4 +32,7 @@ elementToStart.addEventListener("click", () =>{
 });
 elementToAbout.addEventListener("click", () =>{
     window.scrollTo({top: 460, behavior: "smooth"});
+});
+elementToContact.addEventListener("click", () =>{
+    window.scrollTo({top: 710, behavior: "smooth"});
 });
