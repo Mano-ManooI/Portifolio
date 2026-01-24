8f313f48ae51b19a28e756733f0814fc14bc246a
Mano-ManooI <cyfgghgihh1@gmail.com>
2026-01-24 18:52:18 -0300

    'login' finalizado

--- Tela de Login/src/script.js
+++ Tela de Login/src/script.js
@@ -2,27 +2,36 @@ const possibilityButton = document.getElementById("possibility");
 const enterAccount = document.getElementById("enterAccount");
 const input1 = document.getElementById("email");
 const input2 = document.getElementById("pin");
+input1.addEventListener("input", (event) =>{
+    let valor = event.target.value;
+    valor = valor.replace(/[^a-zA-Z0-9!?&@.]/g, "");
+    event.target.value = valor;
+});
 
 function possibility(){
-    const title = document.getElementById("title");
+    const titleTag = document.getElementById("title");
+    const informationAccount = document.getElementById("informationAccount");
 
     if(document.title === "Login"){
         input1.value = "";
         input2.value = "";
-        title.innerText = "Criar Nova Conta";
+        informationAccount.innerText = "Conta criada com sucesso!";
+        titleTag.innerText = "Criar Nova Conta";
         document.title = "Sign Up";
         enterAccount.innerHTML = "Cadastrar";
         possibilityButton.innerHTML = "Já possui uma conta?";
 
     }else{
-        input1.value = "";
-        input2.value = "";
-        title.innerText = "Entrar na Conta"
+        informationAccount.innerText = "Conta acessada com sucesso!";
+        titleTag.innerText = "Entrar na Conta";
         document.title = "Login";
         enterAccount.innerHTML = "Entrar";
-        possibilityButton.innerHTML = "Ainda não tem uma conta?";
+        possibilityButton.innerHTML = "Ainda não tem uma conta?";        
     };
 };
+
+const emailValue = document.getElementById("emailValue");
+const pinValue = document.getElementById("pinValue");
 
 function register(){
     let email = input1.value.trim();
@@ -33,12 +42,35 @@ function register(){
         return;
     };
 
-    console.log(`email: ${email} pin: ${pin}`)
+    emailValue.innerText = email;
+    pinValue.innerText = pin;
+    
+    const registerTag = document.getElementById("register");
+    registerTag.style.display = "none";
+    const popUp = document.getElementById("popUp");
+    popUp.style.display = "flex";
 
     input1.value = "";
     input2.value = "";
 }
 
+const buttonCopy = document.getElementById("buttonCopy");
+function copyElements(){
+    const elementsAccount = "Email: " + emailValue.textContent + "; \Senha: " + pinValue.textContent;
+    navigator.clipboard.writeText(elementsAccount);
+
+    buttonCopy.innerHTML = 'Texto Copiado <i class="fa-solid fa-check"></i>';
+    buttonCopy.style.borderRadius = "20px";
+    buttonCopy.style.margin = "-15% -53% 10px 0";
+    setTimeout(() =>{
+        buttonCopy.innerHTML = '<i class="fa-regular fa-copy"></i>';
+        buttonCopy.style.borderRadius = "50%";
+        buttonCopy.style.margin = "-15% -85% 10px 0";
+    }, 2500);
+};
+
+const reloadPage = document.getElementById("reloadPage");
+const credits = document.getElementById("credits");
 window.addEventListener("DOMContentLoaded", (e) =>{
     e.preventDefault();
 
@@ -48,5 +80,14 @@ window.addEventListener("DOMContentLoaded", (e) =>{
     
     enterAccount.addEventListener("click", () =>{
         register();
+    });
+    reloadPage.addEventListener("click", () =>{
+        location.reload();
+    });
+    buttonCopy.addEventListener("click", () =>{
+        copyElements();
+    });
+    credits.addEventListener("click", () =>{
+        open("https://github.com/Mano-ManooI");
     });
 });
