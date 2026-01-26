7f8edc53e1d5deda142200c7b2f0c47982bf26e5
Mano-ManooI <cyfgghgihh1@gmail.com>
2026-01-26 12:10:13 -0300

    script inicializado

--- Calculadora/src/script.js
+++ Calculadora/src/script.js
@@ -1,3 +1,34 @@
 const visor = document.getElementById("visor");
 visor.innerText = "0";
 
+const buttons = document.querySelectorAll("button");
+function confgButtons(button){
+    const clickUser = button.innerText;
+    const nowValue = visor.innerText;
+
+    if(clickUser === "C"){
+        visor.innerText = "0";
+    }else if(nowValue === "0"){
+        if(clickUser === "0"){
+            visor.innerText = "0";
+        }else{
+            visor.innerText = clickUser;
+        };
+    }else if(clickUser === ""){
+        
+
+    }else{
+        visor.innerText += clickUser
+    }
+};
+
+
+window.addEventListener("DOMContentLoaded", (e) =>{
+    e.preventDefault();
+
+    buttons.forEach(button =>{
+        button.addEventListener("click", () =>{
+            confgButtons(button);
+        });
+    });
+});
