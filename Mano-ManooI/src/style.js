13e9ed83ed3cc81df7611ccbbddddf1eddaf722d
Mano-ManooI <cyfgghgihh1@gmail.com>
2026-01-31 17:37:01 -0300

    resete no projeto

--- Mano-ManooI/src/style.js
+++ Mano-ManooI/src/style.js
@@ -1,47 +1,32 @@
-const header = document.querySelector("header");
+const toggleColorButton = document.getElementById("toggleColor");
 
-const elementToStart = document.getElementById("toStart");
-const elementToAbout = document.getElementById("toAbout");
-const elementToContact = document.getElementById("toContact");
-
-const textStart = "Início";
-const textAbout = "Sobre";
-const textContact = "Contato";
-const iconStart = `<i class="fa-regular fa-house"></i>`;
-const iconAbout = `<i class="fa-solid fa-info"></i>`;
-const iconContact = `<i class="fa-brands fa-whatsapp"></i>`;
-
-window.addEventListener("scroll", () =>{
-    if(window.scrollY > 150){
-        header.style.width = "25%";
-        header.style.background = "#f9f9f9";
-        elementToStart.innerHTML = iconStart;
-        elementToAbout.innerHTML = iconAbout;
-        elementToContact.innerHTML = iconContact;
-    }else{
-        header.style.width = "75%";
-        header.style.background = "rgb(235, 235, 255)";
-        elementToStart.innerHTML = textStart;
-        elementToAbout.innerHTML = textAbout;
-        elementToContact.innerHTML = textContact;
-    };
-});
-
-window.addEventListener("DOMContentLoaded", () =>{
-    elementToStart.addEventListener("click", () => {
-        if (window.scrollY === 0) {
-            window.scrollTo({ top: 15, behavior: "smooth" });
-            setTimeout(() => {
-                window.scrollTo({ top: 0, behavior: "smooth" });
-            }, 100);
-        } else {
-            window.scrollTo({ top: 0, behavior: "smooth" });
+function toggleColor(){
+    const darkMode = "darkMode";
+    const lightMode = "lightMode";
+    const elementsDark = document.querySelectorAll(".darkMode");
+    const elementsLight = document.querySelectorAll(".lightMode");
+    elementsDark.forEach((element) =>{
+        if(element.classList.contains(darkMode)){
+            element.classList.remove(darkMode);
+            element.classList.add(lightMode);
         };
     });
-    elementToAbout.addEventListener("click", () => {
-        window.scrollTo({ top: 460, behavior: "smooth" });
+    elementsLight.forEach((element) => {
+        if (element.classList.contains(lightMode)) {
+            element.classList.remove(lightMode);
+            element.classList.add(darkMode);
+        };
     });
-    elementToContact.addEventListener("click", () => {
-        window.scrollTo({ top: 710, behavior: "smooth" });
+
+    if(toggleColorButton.innerHTML === `<i class="fa-regular fa-sun"></i>`){
+        toggleColorButton.innerHTML = `<i class="fa-regular fa-moon"></i>`;
+    }else{
+        toggleColorButton.innerHTML = `<i class="fa-regular fa-sun"></i>`;
+    };
+};
+
+window.addEventListener("DOMContentLoaded", () =>{
+    toggleColorButton.addEventListener("click", () => {
+        toggleColor();
     });
 });
