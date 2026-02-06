db103b9379bb5c8f1ce4d6ff8926cec4e7ad8900
Mano-ManooI <cyfgghgihh1@gmail.com>
2026-02-06 11:28:33 -0300

    configuração em 'apagar'

--- Calculadora/src/script.js
+++ Calculadora/src/script.js
@@ -4,28 +4,26 @@ visor.innerText = "0";
 const buttons = document.querySelectorAll("button");
 function confgButtons(button){
     const clickUser = button.innerText;
-    const nowValue = visor.innerText;
+    const nowValue = visor.textContent;
 
     if(clickUser === "C"){
         visor.innerText = "0";
+    }else if(clickUser.trim() === ""){
+        if(nowValue.length <= 1 || nowValue === "0"){
+            visor.innerText = "0";
+        }else{
+            visor.innerText = visor.innerText.slice(0, -1);
+        }
     }else if(nowValue === "0"){
         if(clickUser === "0"){
             visor.innerText = "0";
         }else{
             visor.innerText = clickUser;
         };
-    }else if(clickUser === "←"){
-        if(nowValue === "0" || nowValue === ""){
-            visor.innerText = "0";
-        }else{
-            visor.innerText = visor.innerText.slice(0, -1);
-        };
-
     }else{
-        visor.innerText += clickUser
-    }
+        visor.innerText += clickUser;
+    };
 };
-
 
 window.addEventListener("DOMContentLoaded", (e) =>{
     e.preventDefault();
