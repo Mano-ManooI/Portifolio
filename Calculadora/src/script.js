2e67ce804349fab52efff6531f897ec390bbcb63
Mano-ManooI <cyfgghgihh1@gmail.com>
2026-02-09 17:17:03 -0300

    limite de input adicionado

--- Calculadora/src/script.js
+++ Calculadora/src/script.js
@@ -8,28 +8,33 @@ function confgButtons(button){
 
     if(clickUser === "C"){
         visor.innerText = "0";
+        return;
     }else if(clickUser.trim() === ""){
         if(nowValue.length <= 1 || nowValue === "0"){
             visor.innerText = "0";
+            return;
         }else{
             visor.innerText = visor.innerText.slice(0, -1);
-        }
+        };
     }else if(nowValue === "0"){
         if(clickUser === "0"){
             visor.innerText = "0";
+            return;
         }else{
             visor.innerText = clickUser;
         };
+    }else if(visor.textContent.length >= 11) {
+        return;
     }else{
         visor.innerText += clickUser;
     };
 };
 
-window.addEventListener("DOMContentLoaded", (e) =>{
-    e.preventDefault();
-
+window.addEventListener("DOMContentLoaded", () =>{
     buttons.forEach(button =>{
-        button.addEventListener("click", () =>{
+        button.addEventListener("click", (e) =>{
+            e.preventDefault();
+
             confgButtons(button);
         });
     });
