d6ac3c9b6566f5dedc7bb44b69508e991928d570
Mano-ManooI <cyfgghgihh1@gmail.com>
2026-02-10 15:52:44 -0300

    IMC iniciado

---
+++ IMC/src/script.js
@@ -0,0 +1,12 @@
+const input1 = document.getElementById("height");
+const input2 = document.getElementById("weight");
+
+window.addEventListener("DOMContentLoaded", () =>{
+    input1.addEventListener("input", () => {
+        
+    });
+    
+    input2.addEventListener("input", () =>{
+        
+    });
+});
