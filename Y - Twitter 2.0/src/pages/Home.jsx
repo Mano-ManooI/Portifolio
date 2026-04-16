f3447e5a2017bb492977ee6ca76f6b2ebf8c2558
Mano-ManooI <cyfgghgihh1@gmail.com>
2026-04-16 16:15:50 -0300

    Remove componente Aside e suas referências, ajustando a estrutura da página inicial e da página de erro 404

--- Y - Twitter 2.0/src/pages/Home.jsx
+++ Y - Twitter 2.0/src/pages/Home.jsx
@@ -1,11 +1,9 @@
 import Header from "../components/Header";
-import Aside from "../components/Aside";
 
 function Home(){
     return(
         <div>
             <Header />
-            <Aside />
             <main>
                 <h1>Página Inicial</h1>
             </main>
